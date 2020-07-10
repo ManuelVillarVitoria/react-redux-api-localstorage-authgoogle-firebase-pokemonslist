83b2f3332677ee3606ef2a0bcb41f893c0ad6e6f
manu <manu.villar.vitoria@gmail.com>
2020-07-10 16:21:24 +0200

    setting up store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,16 @@
-//With the dispatch we activate  the reducer and with getState we obtain the Initialdata
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+
+import pokeReducer from './pokeDucks'
+
+const rootReducer = combineReducers({
+    pokemones: pokeReducer
+})
+
+//https://github.com/zalmoxisus/redux-devtools-extension#12-advanced-store-setup
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default function generateStore() {
+    const store = createStore( rootReducer, composeEnhancers ( applyMiddleware(thunk) ))
+    return store;
+}
