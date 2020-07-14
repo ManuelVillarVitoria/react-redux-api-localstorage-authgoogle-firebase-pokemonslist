4437f3ea7bf746bb1410033327a582e3ddb882e1
manu <manu.villar.vitoria@gmail.com>
2020-07-14 21:00:05 +0200

    pushing the user to the 'Inicio'

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import {ingresoUsuarioAccion } from '../redux/usuarioDucks'
 
-const Login = () => {
+import { withRouter } from 'react-router-dom'
+
+const Login = (props) => {
 
     const dispatch = useDispatch()
-    
+
+    const loading = useSelector(store => store.usuario.loading)
+    const activo = useSelector(store => store.usuario.activo)
+    console.log(activo)
+
+    React.useEffect(() => {
+        console.log(activo)
+        if(activo) {
+            props.history.push('/')
+        }
+    }, [activo])
+
     return (
         <div className="mt-5 text-center">
             <h3>Ingreso con Google</h3>
@@ -13,10 +26,11 @@ const Login = () => {
             <button 
                 className="btn btn-dark"
                 onClick={() => dispatch(ingresoUsuarioAccion()) }
+                disabled={loading}
             >Acceder
             </button>
         </div>
     )
 }
 
-export default Login
+export default withRouter(Login)
